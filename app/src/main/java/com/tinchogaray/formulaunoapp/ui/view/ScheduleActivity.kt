0487371bd80f92ac75372b091b8e5761135d26e6
martin garay <garaym98@hotmail.com>
2023-02-08 22:45:48 -0300

    Se agrega la poc de un dropdwon
    Queda pendiente en el proximo commit hacer un refactor de toda la logica para que sea acorde a la arquitectura

--- app/src/main/java/com/tinchogaray/formulaunoapp/ui/view/ScheduleActivity.kt
+++ app/src/main/java/com/tinchogaray/formulaunoapp/ui/view/ScheduleActivity.kt
@@ -1,10 +1,14 @@
 package com.tinchogaray.formulaunoapp.ui.view
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.tinchogaray.formulaunoapp.R
 import com.tinchogaray.formulaunoapp.databinding.ActivitySchedulesListBinding
 import com.tinchogaray.formulaunoapp.domain.model.RaceSchedule
 import com.tinchogaray.formulaunoapp.ui.adapter.ScheduleAdapter
@@ -19,6 +23,9 @@ class ScheduleActivity : AppCompatActivity() {
 
     private val raceScheduleViewModel: ScheduleViewModel by viewModels()
     private var raceSchedule = mutableListOf<RaceSchedule>()
+    private lateinit var autocomplete: AutoCompleteTextView
+    private lateinit var arrayAdapter: ArrayAdapter<String>
+    private var year = mutableListOf<String>("2022", "2021", "2015")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +34,13 @@ class ScheduleActivity : AppCompatActivity() {
         setObserver()
         raceScheduleViewModel.getYearRaceSchedule()
         initRecyclerView()
+
+        autocomplete = binding.dropdownYearMenu.autoCompleteYearTxt
+        arrayAdapter = ArrayAdapter(this, R.layout.year_list_item_dropdown, year)
+        autocomplete.setAdapter(arrayAdapter)
+        autocomplete.setOnItemClickListener { parent, view, position, id ->
+            val item = parent.getItemAtPosition(position).toString()
+        }
     }
 
     private fun initRecyclerView() {
