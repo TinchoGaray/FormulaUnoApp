a529e9656b1d960d1e2b8c16c0e772b22afadddf
martin garay <garaym98@hotmail.com>
2023-02-21 20:16:30 -0300

    Mejora en el diseño
    Se hacen mejoras en el diseño agregando imagenes en webp, assets y se agrega una logica para colocar imagenes de las carreras

--- app/src/main/java/com/tinchogaray/formulaunoapp/ui/viewholder/ScheduleViewHolder.kt
+++ app/src/main/java/com/tinchogaray/formulaunoapp/ui/viewholder/ScheduleViewHolder.kt
@@ -23,5 +23,77 @@ class ScheduleViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         country.text = raceSchedule.circuit.location.country
         circuitName.text = raceSchedule.circuit.circuitName
         raceName.text = raceSchedule.raceName
+        renderRaceImage(raceSchedule.raceName)
+    }
+
+    private fun renderRaceImage(raceName: String) {
+        when (raceName) {
+            "Bahrain Grand Prix" -> {
+                scheduleRaceImage.setImageResource(R.drawable.bahrain_grand_prix)
+            }
+            "Saudi Arabian Grand Prix" -> {
+                scheduleRaceImage.setImageResource(R.drawable.saudi_arabian_grand_prix)
+            }
+            "Australian Grand Prix" -> {
+                scheduleRaceImage.setImageResource(R.drawable.australian_grand_prix)
+            }
+            "Emilia Romagna Grand Prix" -> {
+                scheduleRaceImage.setImageResource(R.drawable.emilia_romagna_grand_prix)
+            }
+            "Miami Grand Prix" -> {
+                scheduleRaceImage.setImageResource(R.drawable.miami_grand_prix)
+            }
+            "Spanish Grand Prix" -> {
+                scheduleRaceImage.setImageResource(R.drawable.spanish_grand_prix)
+            }
+            "Monaco Grand Prix" -> {
+                scheduleRaceImage.setImageResource(R.drawable.monaco_grand_prix)
+            }
+            "Azerbaijan Grand Prix" -> {
+                scheduleRaceImage.setImageResource(R.drawable.azerbaijan_grand_prix)
+            }
+            "Canadian Grand Prix" -> {
+                scheduleRaceImage.setImageResource(R.drawable.canadian_grand_prix)
+            }
+            "British Grand Prix" -> {
+                scheduleRaceImage.setImageResource(R.drawable.british_grand_prix)
+            }
+            "Austrian Grand Prix" -> {
+                scheduleRaceImage.setImageResource(R.drawable.austrian_grand_prix)
+            }
+            "French Grand Prix" -> {
+                scheduleRaceImage.setImageResource(R.drawable.french_grand_prix)
+            }
+            "Hungarian Grand Prix" -> {
+                scheduleRaceImage.setImageResource(R.drawable.hungarian_grand_prix)
+            }
+            "Belgian Grand Prix" -> {
+                scheduleRaceImage.setImageResource(R.drawable.belgian_grand_prix)
+            }
+            "Dutch Grand Prix" -> {
+                scheduleRaceImage.setImageResource(R.drawable.dutch_grand_prix)
+            }
+            "Italian Grand Prix" -> {
+                scheduleRaceImage.setImageResource(R.drawable.italian_grand_prix)
+            }
+            "Singapore Grand Prix" -> {
+                scheduleRaceImage.setImageResource(R.drawable.singapore_grand_prix)
+            }
+            "Japanese Grand Prix" -> {
+                scheduleRaceImage.setImageResource(R.drawable.japanese_grand_prix)
+            }
+            "United States Grand Prix" -> {
+                scheduleRaceImage.setImageResource(R.drawable.united_states_grand_prix)
+            }
+            "Mexico City Grand Prix" -> {
+                scheduleRaceImage.setImageResource(R.drawable.mexico_city_grand_prix)
+            }
+            "Brazilian Grand Prix" -> {
+                scheduleRaceImage.setImageResource(R.drawable.brazilian_grand_prix)
+            }
+            "Abu Dhabi Grand Prix" -> {
+                scheduleRaceImage.setImageResource(R.drawable.abu_dhabi_grand_prix)
+            }
+        }
     }
 }
